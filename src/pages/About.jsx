7728ac9557476bd5d3a9112c5bed618fbad31e41
Suraj <skbindpurwa@gmail.com>
2026-04-27 22:46:18 +0530

    fix base path issue

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,21 +2,24 @@ import React from "react";
 
 const About = () => {
   return (
-    <section id="about" className="py-35 px-25 bg-gray-900/97">
-      <div className="max-w-6xl mx-auto grid md:grid-cols-2 items-center">
+    <section id="about" className="py-24 px-6 bg-gray-900/97">
+      <div className="max-w-6xl mx-auto grid md:grid-cols-2 items-center gap-10">
+        
         {/* Profile Image */}
         <div className="flex justify-center md:justify-start">
           <img
             src="/Sk/sk1.jpg"
             alt="Suraj Photo"
-            className="w-58 h-74 object-cover mx-auto shadow-lg rounded-[50%/55%]"
+            className="w-[230px] h-[300px] object-cover mx-auto shadow-lg rounded-[50%/55%]"
             data-aos="zoom-in"
           />
         </div>
 
         {/* Text Content */}
         <div className="text-center md:text-left" data-aos="fade-left">
-          <h3 className="text-gray-300 text-3xl font-bold mb-4">Who Am I?</h3>
+          <h3 className="text-gray-300 text-3xl font-bold mb-4">
+            Who Am I?
+          </h3>
           <p className="text-gray-300 text-base md:text-lg">
             Passionate developer with experience in both frontend and backend.
             Expert in building scalable apps, responsive UI, and smooth user
@@ -24,6 +27,7 @@ const About = () => {
             continuous improvement.
           </p>
         </div>
+
       </div>
     </section>
   );
