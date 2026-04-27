05bf2e2ce721b4658e52f8ef2cb6df4c762262e3
Suraj <skbindpurwa@gmail.com>
2026-04-27 22:43:15 +0530

    fix base path issue

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,18 +12,19 @@ const Projects = () => {
         </h2>
 
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-12">
+          
           {/* Project 1 */}
           <div
-            className="w-full max-w-xs flex flex-col items-center justify-center mx-auto bg-blue-400 rounded-lg shadow-lg overflow-hidden transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
+            className="w-full max-w-xs flex flex-col items-center justify-center mx-auto bg-blue-400 rounded-lg shadow-lg overflow-hidden transition-all duration-300 hover:scale-105 hover:shadow-2xl"
             data-aos="fade-up"
             data-aos-delay="100"
           >
             <img
-              src="/My-Portfolio-Website/logos/cloudy.png"
-              // alt="Portfolio Website"
-              className="size-20 object-center block mx-auto"
+              src="/logos/cloudy.png"
+              alt="Weather App"
+              className="w-[80px] h-[80px] object-contain mt-4"
             />
-            <div className="p-6">
+            <div className="p-6 text-center">
               <h3 className="text-xl font-semibold text-gray-900 mb-4">
                 Weather App
               </h3>
@@ -36,16 +37,16 @@ const Projects = () => {
 
           {/* Project 2 */}
           <div
-            className="w-full max-w-xs flex flex-col items-center justify-center mx-auto bg-blue-400 rounded-lg shadow-lg overflow-hidden transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
+            className="w-full max-w-xs flex flex-col items-center justify-center mx-auto bg-blue-400 rounded-lg shadow-lg overflow-hidden transition-all duration-300 hover:scale-105 hover:shadow-2xl"
             data-aos="fade-up"
             data-aos-delay="200"
           >
             <img
-              src="/My-Portfolio-Website/logos/rgb-svgrepo-com.svg"
-              // alt="Space Game"
-              className="size-20 object-center block mx-auto"
+              src="/logos/rgb-svgrepo-com.svg"
+              alt="Gradient Generator"
+              className="w-[80px] h-[80px] object-contain mt-4"
             />
-            <div className="p-6">
+            <div className="p-6 text-center">
               <h3 className="text-xl font-semibold text-gray-900 mb-4">
                 Generate CSS Color Gradient
               </h3>
@@ -59,16 +60,16 @@ const Projects = () => {
 
           {/* Project 3 */}
           <div
-            className="w-full max-w-xs flex flex-col items-center justify-center mx-auto bg-blue-400 rounded-lg shadow-lg overflow-hidden transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
+            className="w-full max-w-xs flex flex-col items-center justify-center mx-auto bg-blue-400 rounded-lg shadow-lg overflow-hidden transition-all duration-300 hover:scale-105 hover:shadow-2xl"
             data-aos="fade-up"
             data-aos-delay="300"
           >
             <img
-              src="/My-Portfolio-Website/logos/users-solid.svg"
-              // alt="Employee Management System"
-              className="size-10 object-center block mx-auto"
+              src="/logos/users-solid.svg"
+              alt="Employee Management System"
+              className="w-[70px] h-[70px] object-contain mt-4"
             />
-            <div className="p-6">
+            <div className="p-6 text-center">
               <h3 className="text-xl font-semibold text-gray-900 mb-3">
                 Employee Management System
               </h3>
@@ -79,6 +80,7 @@ const Projects = () => {
               </p>
             </div>
           </div>
+
         </div>
       </div>
     </section>
